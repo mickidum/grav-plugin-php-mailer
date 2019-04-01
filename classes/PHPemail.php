45834e5c914c19769c26709329763ca18ca1a1b0
Michael <michael@zur.local>
2019-04-01 11:09:18 +0300

    bugs fix

--- classes/PHPemail.php
+++ classes/PHPemail.php
@@ -17,33 +17,37 @@ class PHPemail
     {
         if ($this->message && is_array($this->message)) {
 
-            $message_header = "<table style='direction:rtl; border: dotted 1px #bfbfbf;'><tr><th colspan='2'><h2 style='margin:0;'><strong>New message from your website</strong></h2></th></tr>";
-            $message_footer = "</table>";     
-            $message = "";
-
             $to = $this->message['to']['mail'];
             $from = $this->message['from']['mail'];
             $from_name = $this->message['from']['name'];
-            $subject = $this->message['subject'];
+            $subject = $this->message['subject'] ? $this->message['subject'] : 'New message from your website';
             $body = $this->message['body'];
 
+            $message_header = "<table style='border: dotted 1px #bfbfbf;'><tr><th colspan='2'><h2 style='margin:0;'><strong>{$subject}</strong></h2></th></tr>";
+            $message_footer = "</table>";     
+            $message = "";
+
             if (!$body) {
-                die('<div class="notices error red">
-                    <p>Error while sending mail</p>
-                </div>');
+                die("error while sending mail");
             }
 
             $headers = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/html; charset=UTF-8' . "\r\n";
             $headers .= 'From:"'.$from_name.'" <'.$from.'>';
 
-            $message .= "<tr><th style='padding: 10px;border-top:dotted 1px #000;border-left:dotted 1px #000;'><strong>Info</strong></th><td style='padding:10px;border-top:dotted 1px #000;'>{$body}</td>";
+            // echo $headers;
+            // echo $to;
+
+            $message .= "<tr><th style='padding: 10px;border-top:dotted 1px #000;border-left:dotted 1px #000;'><strong>Details</strong></th><td style='padding:10px;border-top:dotted 1px #000;'>{$body}</td>";
             $message = $message_header.$message.$message_footer;
-            
+            // echo $message;
+            // echo 'sent successful';
             try {
                 mail($to, $subject, $message, $headers);
             } catch (Exception $e) {
                 echo "error sending mail";
             }
+            
         }
     }
+
 }
